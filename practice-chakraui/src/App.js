a2472e46cac1c88a2b8fc0224fbcfc4fa35164ab
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2023-08-09 17:06:55 -0300

    se creo carpeta /resources/images

--- practice-chakraui/src/App.js
+++ practice-chakraui/src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import {
   ChakraProvider,
-  Box
+  Box,
+  Button,
+  Heading
 } from '@chakra-ui/react';
-//import { ColorModeSwitcher } from './ColorModeSwitcher';
+// import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { theme } from './resources';
-
+import planta from './resources/images/planta.png'
 function App() {
   return (
     <ChakraProvider theme={theme}>
           {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
-          <Box>CONTENIDO</Box>
+          <Box h='40vh' bg='gray' bgImage={`url(${planta})`} bgRepeat="no-repeat">
+            <Heading
+              color='white'
+              textTransform='uppercase'
+              textAlign='center'
+            >Domina el terreno</Heading>
+              <Button variant='outline' >Ver detalles</Button>
+              <Button variant='outline' >Ver Video</Button>
+              
+          </Box>
     </ChakraProvider>
   );
 }
