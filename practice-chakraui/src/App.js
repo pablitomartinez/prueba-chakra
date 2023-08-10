e5ee82d701acc4a560142c1b65ad76b9d14eda75
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2023-08-10 12:46:31 -0300

    styles sin theme(sin configuracion global)

--- practice-chakraui/src/App.js
+++ practice-chakraui/src/App.js
@@ -3,25 +3,82 @@ import {
   ChakraProvider,
   Box,
   Button,
-  Heading
+  Heading,
+  Flex,
+  Stack,
 } from '@chakra-ui/react';
 // import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { theme } from './resources';
-import planta from './resources/images/planta.png'
+import planta from './resources/images/planta.png';
 function App() {
   return (
     <ChakraProvider theme={theme}>
-          {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
-          <Box h='40vh' bg='gray' bgImage={`url(${planta})`} bgRepeat="no-repeat">
-            <Heading
-              color='white'
-              textTransform='uppercase'
-              textAlign='center'
-            >Domina el terreno</Heading>
-              <Button variant='outline' >Ver detalles</Button>
-              <Button variant='outline' >Ver Video</Button>
-              
-          </Box>
+      {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
+      <Box
+        h="40vh"
+        bg="gray"
+        bgImage={`url(${planta})`}
+        bgRepeat="no-repeat"
+        bgSize="cover"
+      >
+        <Flex
+          direction="column"
+          alignItems="center"
+          justify="center"
+          h="100%"
+          bg="rgb(0 0 0 / 50%)"
+          p={['0 10%', null, '0 20%']}
+        >
+          <Heading
+            color="white"
+            textTransform="uppercase"
+            textAlign="center"
+            fontWeight="light"
+            letterSpacing="5px"
+          >
+            domina el terreno
+          </Heading>
+          <Stack 
+            direction={{
+              base:'column',
+              md:'row'
+            }} spacing="40px" mt="30px"
+            w={['100%', null , 'auto']}
+            
+            >
+            <Button
+              variant="outline"
+              fontWeight="light"
+              borderRadius="0"
+              color="white"
+              size="lg"
+              textTransform="uppercase"
+              letterSpacing="1px"
+              _hover={{
+                color: 'black',
+                bg: 'white',
+              }}
+            >
+              cuidar
+            </Button>
+            <Button
+              variant="outline"
+              fontWeight="light"
+              borderRadius="0"
+              color="white"
+              size="lg"
+              textTransform="uppercase"
+              letterSpacing="1px"
+              _hover={{
+                color: 'black',
+                bg: 'white',
+              }}
+            >
+              valorar
+            </Button>
+          </Stack>
+        </Flex>
+      </Box>
     </ChakraProvider>
   );
 }
